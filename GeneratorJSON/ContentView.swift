35d5c1581431f31ba930b6c297b095486c322358
Marat Gazizov <gazizivmf@gmail.com>
2021-03-04 09:01:52 +0300

    Initial Commit

---
+++ GeneratorJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeneratorJSON
+//
+//  Created by Marat Gazizov on 04.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeneratorJSON/GeneratorJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneratorJSONApp.swift
+//  GeneratorJSON
+//
+//  Created by Marat Gazizov on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct GeneratorJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
